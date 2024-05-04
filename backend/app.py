af41ff63b9a674813b4e3cfcfc693e4c969b6594
SameerJahagirdar <86760141+SameerJahagirdar@users.noreply.github.com>
2024-05-04 13:49:10 +0530

    added upload feature

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,18 @@
 import time
 import cv2 
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response,request,jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-def gen_dynamic(video_path):
-    """Video streaming generator function."""
-    cap = cv2.VideoCapture(video_path)
 
+
+
+def gen(video_path):
+    """Video streaming generator function."""
     
-    while(cap.isOpened()):
-        ret, img = cap.read()
-        if ret == True:
-            img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
-            frame = cv2.imencode('.jpg', img)[1].tobytes()
-            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        else: 
-            break
-
-def gen(opt):
-    """Video streaming generator function."""
-    if opt == "T":
-        video_path = "D:/MitMetro/newData2/Ticketing_Line_1.avi" 
-    if opt == "E":
-        video_path = "D:/MitMetro/newdata1/FallOnEscalator_4.avi"
-    if opt == "P":
-        video_path = "D:/MitMetro/newdata1/PF_EdgeCrossing_6.avi"
-    if opt == "L":
-        video_path = "D:/MitMetro/newdata1/LeftObject_2.avi"
     
     cap = cv2.VideoCapture(video_path)
 
@@ -53,36 +36,44 @@ def gen(opt):
 @app.route('/video_feed/platform')
 def video_feed_platform():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen("P"),
+    file = os.listdir(os.path.join(os.getcwd(),'videos','Platform'))[0]
+    file = os.path.join(os.getcwd(),'videos','Platform',file)
+    print(file)
+    
+    return Response(gen(file),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/video_feed/ticket')
 def video_feed_ticket():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen("T"),
+    file = os.listdir(os.path.join(os.getcwd(),'videos','Abandon'))[0]
+    file = os.path.join(os.getcwd(),'videos','Abandon',file)
+    print(file)
+    return Response(gen(file),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/video_feed/lostbaggage')
 def video_feed_lostbaggage():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen("L"),
+    file = os.listdir(os.path.join(os.getcwd(),'videos','Queue'))[0]
+    file = os.path.join(os.getcwd(),'videos','Queue',file)
+    print(file)
+    return Response(gen(file),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/video_feed/<option>', methods=['POST'])
-def video_feed(option):
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    if option == 'escalator':
-        video_path = "D:/MitMetro/newdata1/FallOnEscalator_4.avi"
-    elif option == 'platform':
-        video_path = "D:/MitMetro/newdata1/PF_EdgeCrossing_6.avi"
-    elif option == 'ticket':
-        video_path = "D:/MitMetro/newData2/Ticketing_Line_1.avi"
-    elif option == 'lostbaggage':
-        video_path = "D:/MitMetro/newdata1/LeftObject_2.avi"
-    else:
-        return Response("Invalid option", status=400)
-
-    return Response(gen_dynamic(video_path), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed/upload',methods=['POST'])
+def video_feed_upload():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+    file = request.files['file']
+    video_type = request.form['type']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+    if file:
+        filename = file.filename
+        file.save(os.path.join(os.getcwd(),'videos',video_type, filename))
+        return jsonify({'message': 'File uploaded successfully', 'filename': filename})
+    
 
 
 if __name__ == '__main__':
--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -5,6 +5,7 @@ import Escalator from './escalator';
 import LostBaggage from './lostBaggage';
 import PlatformEdge from './platformEdge';
 import TicketQueue from './ticketQueue';
+import axios from 'axios';
 
 
 const Home = () => {
@@ -12,11 +13,56 @@ const Home = () => {
     const handleTabSwitch = (value) =>{
         setTab(value)
     }
+    const [file, setFile] = useState(null);
+    const [type,setType] = useState("");
+    const [uploadProgress, setUploadProgress] = useState(0);
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+    const handleTypeChange = (e) => {
+        setType(e.target.value);
+    };
+
+    const handleUpload = async () => {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('type',type)
+
+        try {
+            const res = await axios.post('http://127.0.0.1:5000/video_feed/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                onUploadProgress: (progressEvent) => {
+                    const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                    setUploadProgress(percentCompleted);
+                }
+            });
+            console.log(res.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
     useEffect(()=>{
         
     },[tab])
     return (
         <>
+        <div>
+            <input type="file" name="Upload" id="" onChange={handleFileChange} />
+            <select name="" id="" onChange={handleTypeChange}>
+                <option value="">Select</option>
+                <option value="Platform">Platform</option>
+                <option value="Queue">Queue</option>
+                <option value="Abandon">Abandon</option>
+            </select>
+            <button onClick={handleUpload}>Upload</button>
+
+            {uploadProgress}
+
+        </div>
             <div className="tabs-grid-container">
                 <div onClick={()=>handleTabSwitch('E')} className="tab">Escalator</div>
                 <div onClick={()=>handleTabSwitch('L')} className="tab">Lost Baggage</div>
